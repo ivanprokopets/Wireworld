443d76970fa8d27bbbf7c2e7db867976cdf291ba
ivanprokopets <ivanprokopets1999@gmail.com>
2019-11-08 14:58:21 +0100

    Add classes and coding for Canvas Panel

--- wireworld/src/main/java/Canvas.java
+++ wireworld/src/main/java/Canvas.java
@@ -19,6 +19,33 @@ public class Canvas extends JPanel {
 
     @Override
     public void paint(Graphics g) {
-
+        super.paint(g);
+        for (int x = 0; x < widht; x++) {
+            for (int y = 0; y < hegth; y++) {
+                if (tabl[x][y] == 0) {
+                    g.setColor(Color.black);
+                    g.fillRect(x * POINT_RADIUS, y * POINT_RADIUS, POINT_RADIUS, POINT_RADIUS);
+                }
+                if (tabl[x][y] == 1) {
+                    Color c1 = new Color(255, 215, 0);
+                    g.setColor(c1);
+                    if (x == 0 || x == 49 || y == 0 || y == 49) {
+                        Color c0 = new Color(92, 184, 92);
+                        g.setColor(c0);
+                    }
+                    g.fillRect(x * POINT_RADIUS, y * POINT_RADIUS, POINT_RADIUS, POINT_RADIUS);
+                }
+                if (tabl[x][y] == 2) {
+                    Color c1 = new Color(255, 64, 0);
+                    g.setColor(c1);
+                    g.fillRect(x * POINT_RADIUS, y * POINT_RADIUS, POINT_RADIUS, POINT_RADIUS);
+                }
+                if (tabl[x][y] == 3) {
+                    Color c2 = new Color(0, 128, 255);
+                    g.setColor(c2);
+                    g.fillRect(x * POINT_RADIUS, y * POINT_RADIUS, POINT_RADIUS, POINT_RADIUS);
+                }
+            }
+        }
     }
 }
--- wireworld/src/main/java/Process.java
+++ wireworld/src/main/java/Process.java
@@ -1,6 +1,68 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Hashtable;
+
 public class Process {
+    final int POINT_RADIUS = 10;
+    final int LIFE_SIZE = 50;
+    final int FIELD_SIZE = (LIFE_SIZE + 1) * POINT_RADIUS + 3;
+    final int BTN_PANEL = 50;
+    final int MOUSE_BUTTON_LEFT = 1;
+    final int MOUSE_BUTTON_RIGHT = 3;
+    int Speed = 350;
+
+    int[][] lifeGeneration = new int[LIFE_SIZE][LIFE_SIZE];
+    int[][] nextGeneration = new int[LIFE_SIZE][LIFE_SIZE];
+    volatile boolean PlayGeneration = false;
+
+    JFrame frame;
+    Canvas canvasPanel;
 
     void go() {
+        frame = new JFrame("WireWorld");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(FIELD_SIZE - 13, FIELD_SIZE + BTN_PANEL + 7);
+        frame.setLocation(450, 100);
+        frame.setResizable(false);
+
+
+        canvasPanel = new Canvas(lifeGeneration, LIFE_SIZE, LIFE_SIZE, canvasPanel, POINT_RADIUS);
+        canvasPanel.setBackground(Color.white);
+
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, 1, 700, 350);
+
+        //Create the label table
+        Hashtable labelTable = new Hashtable();
+        labelTable.put(new Integer(50), new JLabel("Fast"));
+        labelTable.put(new Integer(650), new JLabel("Slow"));
+        slider.setLabelTable(labelTable);
+
+        slider.setPaintLabels(true);
+
+        JButton fillButton = new JButton(GetStart());
+        fillButton.addActionListener(new gra.Process.FillButtonListener());
+
+        JButton dropButton = new JButton(GetName());
+        dropButton.addActionListener(new gra.Process.DropButtonListener());
+
+        JButton saveButton = new JButton(SaveStart());
+        saveButton.addActionListener(new gra.Process.SaveButtonListener());
+
+        JButton readButton = new JButton(ReadStart());
+        readButton.addActionListener(new gra.Process.ReadButtonListener());
+
+        JCheckBox checkBoxRight = new JCheckBox("DiodaRight");
+        JCheckBox checkBoxLeft = new JCheckBox("DiodaLeft");
+
+        final JButton playButton = new JButton("Play");
+        playButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                PlayGeneration = !PlayGeneration;
+                playButton.setText(PlayGeneration ? "Stop" : "Play");
+            }
+        });
 
     }
 }
