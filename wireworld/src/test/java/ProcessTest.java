2a69067517d85d59de07f95220d7ee420d5d6f72
ivanprokopets <ivanprokopets1999@gmail.com>
2019-11-23 20:51:23 +0100

    Finish Version

--- wireworld/src/test/java/ProcessTest.java
+++ wireworld/src/test/java/ProcessTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 public class ProcessTest {
     @Test
     public void go() throws Exception {
-
     }
 
     @Test
@@ -17,4 +16,38 @@ public class ProcessTest {
     private String getName(String name) {
         return name;
     }
+
+    @Test
+    public void getStart() throws Exception {
+        String start = "Start";
+        Assert.assertEquals("Niepoprawny start","Start",getStart(start));
+        System.out.println("The Test getStart was successful");
+    }
+
+    private String getStart(String start) {
+        return start;
+    }
+
+    @Test
+    public void saveStart() throws Exception {
+        String save = "Save";
+        Assert.assertEquals("Niepoprawny save","Save",saveStart(save));
+        System.out.println("The Test saveStart was successful");
+    }
+
+    private String saveStart(String save) {
+        return save;
+    }
+
+    @Test
+    public void readStart() throws Exception {
+        String read ="Read";
+        Assert.assertEquals("Niepoprawny read","Read",readStart(read));
+        System.out.println("The Test readStart was successful");
+    }
+
+    private String readStart(String read) {
+        return read;
+    }
+
 }
